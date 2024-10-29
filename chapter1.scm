5d8dca7f33219c3f337287ba1a3a902b3552dd82
Sean Bix <bixkitts@gmail.com>
2024-10-29 13:13:51 +0100

    more exercises

--- chapter1.scm
+++ chapter1.scm
@@ -86,3 +86,81 @@
 ; order evaluation, causing an infinite recursion.
 ; a normal if conditional is special in that the consequent or
 ; alternative get computer strictly after the predicate.
+
+; 1.7
+;----------------
+(define (sqrt-iter guess previous-guess x)
+  (if (good-enough? guess previous-guess)
+      guess
+      (sqrt-iter (improve guess x)
+                 guess
+                 x)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess previous-guess)
+  (< (abs (- guess previous-guess)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 0 x))
+
+(define answer (sqrt 9))
+(display answer)
+(newline)
+
+; 1.8
+;-----------------
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
+
+(define (cbrt x)
+  (sqrt x))
+
+(define answer (cbrt 27))
+(display answer)
+(newline)
+
+; 1.9
+; ---------------
+; recursive process,
+; because operator + needs to be resolved
+; for every decrement of a,
+; recursively calling inc and adding
+; stack frames before finaly resolving it
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+;(+ 4 5)
+;(inc (+ 3 5))
+;(inc (inc (+ 2 5)))
+;(inc (inc (inc (+ 1 5))))
+;(inc (inc (inc (inc (+ 0 5))))
+;(inc (inc (inc (inc 5)))
+;(inc (inc (inc 6)))
+;(inc (inc 7))
+;(inc 8)
+;9
+
+
+; iterative process,
+; because dec and inc can be applicatively
+; resolved and we can leave this function
+; and go to the next one without needing
+; to keep the stack frame (tail recursion)
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+;(+ 4 5)
+;(+ 3 6)
+;(+ 2 7)
+;(+ 1 8)
+;(+ 0 9)
+;9
